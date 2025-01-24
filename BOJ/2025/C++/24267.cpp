9aa6242a8bf8787d86370d9e68dc9f973101ed20
doil <85823762+novvvv@users.noreply.github.com>
2025-01-24 18:11:52 +0900

    Create 24267.cpp

---
+++ BOJ/2025/C++/24267.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    long long n;
+    cin >> n;
+
+    cout << (n - 2) * (n - 1) * n / 6 << endl;
+    cout << 3 << endl;
+
+    return 0;
+}
+
