cf32e43cc09eaaa2cd522d0126af5cd566a9da52
doil <85823762+novvvv@users.noreply.github.com>
2025-02-04 18:10:29 +0900

    Update Illuminate Buildings.cpp

--- atCoder/Illuminate Buildings.cpp
+++ atCoder/Illuminate Buildings.cpp
@@ -1,34 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-  
+int main(){
+
   int n;
   cin >> n;
-  vector<int> height(n);
+  vector<int> building(n);
   for (int i = 0; i < n; i++) {
-    cin >> height[i];
+    cin >> building[i];
   }
-  
-  int max_building = 1;
+
+  int max_building_cnt = 1;
   for (int i = 0; i < n; i++) {
-    // 높이 i에 대해 간격 i ~ n까지 bruteForce Search
     for (int j = i + 1; j < n; j++) {
-      if (height[i] != height[j]) continue;
-      int height_diff = j - i;
-      int end_point = j;
-      // condition1 : (end_point + height_diff < n) 
-      // condition2 : (height[i] == height[end_point + height_diff])
-      // - i번쨰 높이와 height_diff (높이차)가 동일한 경우까지 반복
-      int cur_max_building = 2;
-      while ((end_point + height_diff < n) && (height[i] == height[end_point + height_diff])) {
-        end_point += height_diff;
-        cur_max_building++;
+      if (building[i] != building[j]) continue;
+      int diff = j - i; // 빌딩 간격
+      int end_ptr = j;
+      int cur_building_cnt = 2;
+      while ((end_ptr + diff < n) && (building[i] == building[end_ptr + diff])) {
+        cur_building_cnt++;
+        end_ptr += diff;
       }
-      max_building = max(max_building, cur_max_building); 
+      max_building_cnt = max(max_building_cnt, cur_building_cnt);
     }
   }
-  
-  cout << max_building << '\n';
-  
+  cout << max_building_cnt << '\n';
+
 }
