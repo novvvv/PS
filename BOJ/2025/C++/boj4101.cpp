7faa585de5a56269a7081fa7c26c14e436c4e9cf
doil <85823762+novvvv@users.noreply.github.com>
2025-03-31 15:12:33 +0900

    Create boj4101.cpp

---
+++ BOJ/2025/C++/boj4101.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std; 
+
+int main() {
+    int a, b;
+    while (true) {
+        cin >> a >> b;
+        if (a == 0 && b == 0) break;
+        if (a > b) cout << "Yes" << endl;
+        else cout << "No" << endl;
+    }
+}
